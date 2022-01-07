8ca865bb7f4b3895944bcb2b189395237b3aafae
antoinebiogeau <72401464+antoinebiogeau@users.noreply.github.com>
2022-01-07 17:43:47 +0100

    Update Credentials.java

--- Personnel/src/jdbc/Credentials.java
+++ Personnel/src/jdbc/Credentials.java
@@ -1,6 +1,6 @@
 package jdbc;
 
-public class CredentialsExample 
+public class Credentials 
 {
 	private static String driver = "mysql";
 	private static String driverClassName = "com.mysql.cj.jdbc.Driver";
