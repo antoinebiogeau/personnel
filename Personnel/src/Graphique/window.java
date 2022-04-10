b6de22595db24805f1b4257ddf9e0304deaa6e43
antoinebiogeau <72401464+antoinebiogeau@users.noreply.github.com>
2022-04-10 15:39:54 +0200

    Interface graphique gestion ligue

---
+++ Personnel/src/Graphique/window.java
@@ -0,0 +1,107 @@
+package Graphique;
+
+import static commandLineMenus.rendering.examples.util.InOut.getString;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import personnel.GestionPersonnel;
+import personnel.SauvegardeImpossible;
+public class window extends Application implements EventHandler<ActionEvent> {
+	public void gg() throws SauvegardeImpossible {
+		GestionPersonnel gestionPersonnel = GestionPersonnel.getGestionPersonnel();
+	}
+	
+	Button ButtonLogin;
+	Button Button2;
+	Stage LoginWindow;
+	TextField password;
+	TextField addligue;
+	Scene scene1, scene2;
+	Button button3;
+	Label label2;
+	Label labellistligue;
+	
+	@Override
+	public void start (Stage primaryStage) throws SauvegardeImpossible{
+		
+
+		LoginWindow = primaryStage;
+		LoginWindow.setTitle("Login");
+		Label label = new Label("Login");
+		//fenetre 1
+		
+		ButtonLogin = new Button("Login");
+		ButtonLogin.setOnAction(this);
+		password = new TextField("");
+		
+		VBox layout1 = new VBox(20);
+		layout1.getChildren().addAll(label,password,ButtonLogin);
+		scene1 = new Scene(layout1,300,250);
+		
+		
+		//fenetre 2
+		label2 = new Label("Menu Ligue");
+		labellistligue = new Label(GestionPersonnel.getGestionPersonnel().getLigues().toString());
+		
+		Button2 = new Button("Back");
+		Button2.setOnAction(this);
+		addligue = new TextField();
+		button3 = new Button("valider");
+		button3.setOnAction(this);
+		
+		VBox layout2 = new VBox(20);
+		layout2.getChildren().addAll(Button2,label2, labellistligue,addligue,button3);
+		scene2 = new Scene(layout2,400,300);
+		
+		LoginWindow.setScene(scene1);
+		LoginWindow.show();
+	}
+
+	public static void main(String[] args) {
+	    launch(args);
+	 }
+
+	@Override
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+		if(event.getSource()==ButtonLogin) {
+			System.out.println(password.getText());
+			try {
+				if(GestionPersonnel.getGestionPersonnel().getRoot().checkPassword(password.getText())) {
+				LoginWindow.setScene(scene2);
+				}
+				else {
+					System.out.println("wrong password");
+				}
+			} catch (SauvegardeImpossible e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if(event.getSource()==Button2) {
+			System.out.println("prout2");
+			LoginWindow.setScene(scene1);
+		}
+		if(event.getSource()==button3) {
+			System.out.println("pouf");
+			try {
+				GestionPersonnel.getGestionPersonnel().addLigue(addligue.getText());
+				labellistligue.setText(GestionPersonnel.getGestionPersonnel().getLigues().toString());
+			} catch (SauvegardeImpossible e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}		
+}
+}
+	
+
